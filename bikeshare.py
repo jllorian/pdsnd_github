6934898678c5b47c3feff833b292d8a16d54309b
José María Llorián Álvarez <88973634+jllorian@users.noreply.github.com>
2022-07-29 20:54:19 +0200

    Update - In line Documentation
    bikeshare.py func returns type corrected

--- bikeshare.py
+++ bikeshare.py
@@ -28,11 +28,11 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (int) month - name of the month to filter by, or "all" to apply no month filter
+        (int) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('\nHello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington).
     while True:
         print('Which city do you want to analyze? Options are: ' + ', '.join(CITY_DATA))
         print(' ')
